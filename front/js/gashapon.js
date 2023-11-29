ff575266eb92a60bd12cca37a781812124489be8
Fiona Hak <fiona.hak@universite-paris-saclay.fr>
2023-11-29 21:15:30 +0100

    anim complete gasha

--- front/js/gashapon.js
+++ front/js/gashapon.js
@@ -1,85 +1,116 @@
-var imageSets = [
-    [
-        '../../assets/images/gesh_lum1.jpg',
-        '../../assets/images/gesh_lum2.jpg',
-        '../../assets/images/gesh_lum3.jpg'
-    ]
-];
-var image4Set = ['../../assets/images/gesh_bout.jpg'];
-var displayInterval = 10;
-var imgElement = document.getElementById('displayedImage');
-var setIndex = 0;
-var index = 0;
-var changeImage = true;
+window.myApp = {}; //éviter la déclaration multiple
 
-setInterval(() => {
-    if (changeImage) {
-        index = (index + 1) % imageSets[setIndex].length;
-        imgElement.src = imageSets[setIndex][index];
-    }
-}, displayInterval);
+window.myApp.lumImages = [
+    '../../assets/images/gesh_lum1.jpg',
+    '../../assets/images/gesh_lum2.jpg',
+    '../../assets/images/gesh_lum3.jpg'
+];
+
+window.myApp.moveImages = [
+    '../../assets/images/gesh_bout.jpg',
+    '../../assets/images/gesh_move1.jpg',
+    '../../assets/images/gesh_move2.jpg',
+    '../../assets/images/gesh_move3.jpg',
+    '../../assets/images/gesh_move4.jpg',
+    '../../assets/images/gesh_move1.jpg',
+    '../../assets/images/gesh_move2.jpg',
+    '../../assets/images/gesh_move3.jpg',
+    '../../assets/images/gesh_move4.jpg',
+    '../../assets/images/gesh_move1.jpg',
+    '../../assets/images/gesh_move2.jpg',
+    '../../assets/images/gesh_move3.jpg',
+    '../../assets/images/gesh_move4.jpg',
+    '../../assets/images/gesh_boule_on.jpg',
+    '../../assets/images/gesh_boule_ok.jpg'
+];
+
+window.myApp.currentIndex = 0;
+window.myApp.intervalId;
+window.myApp.imgElement = document.getElementById('imageDisplay');
+window.myApp.changeImage = true;
+
+function startImageCarousel(images, delay) {
+    window.myApp.intervalId = setInterval(function () {
+        window.myApp.imgElement.src = images[window.myApp.currentIndex];
+        window.myApp.currentIndex = (window.myApp.currentIndex + 1) % images.length;
+    }, delay);
+}
+
+function changeImageFunction() {
+    clearInterval(window.myApp.intervalId);
+    let moveIndex = 0;
+    window.myApp.intervalId = setInterval(function () {
+        window.myApp.imgElement.src = window.myApp.moveImages[moveIndex];
+        moveIndex++;
+        if (moveIndex === window.myApp.moveImages.length) {
+            clearInterval(window.myApp.intervalId);
+        }
+    }, 200);
+}
+
+startImageCarousel(window.myApp.lumImages, 200);
 
 function handleImageClick() {
-    if (changeImage) {
-        imgElement.src = (imgElement.src === image4Set[0]) ? imageSets[setIndex][index] : image4Set[0];
-        changeImage = false;
+    if (window.myApp.changeImage) {
+        changeImageFunction();
+        window.myApp.changeImage = false;
         setTimeout(() => {
-            changeImage = true;
+            window.myApp.changeImage = true;
         }, 3000);
 
         $.ajax({
-        type: "GET",
-        url: config.apiUrl + "/food/choose",
-        dataType: "json",
-        success: function (data) {
-            if (data && data.length > 0) {
-                document.getElementById("displayTypeText").innerText = data[0].type;
-            } else {
-                console.log("Aucune donnée à afficher");
-            }
-        },
-        error: function (data) {
-            console.error("Erreur lors de la requête AJAX : " + data);
-            $("#errorMessage").text("Erreur : " + data.statusText);
-        },
-    });
-
+            type: "GET",
+            url: config.apiUrl + "/food/choose",
+            dataType: "json",
+            success: function (data) {
+                if (data && data.length > 0) {
+                    setTimeout(function () {
+                        document.getElementById("displayTypeText").innerText = data[0].type;
+                        document.getElementById('filterRestoBut').style.display = 'block';
+                    }, 4500);
+                } else {
+                    console.log("Aucune donnée à afficher");
+                }
+            },
+            error: function (data) {
+                console.error("Erreur lors de la requête AJAX : " + data);
+                $("#errorMessage").text("Erreur : " + data.statusText);
+            },
+        });
     }
 }
 
-imgElement.onclick = handleImageClick;
+window.myApp.imgElement.onclick = handleImageClick;
 
 window.addEventListener('beforeunload', () => {
-    changeImage = true;
+    window.myApp.changeImage = true;
 });
 
-// retour page menu
 var returnButton = document.getElementById('returnButton');
 returnButton.addEventListener('click', function () {
-    imgElement.onclick = null;
+    window.myApp.imgElement.onclick = null;
     var footer = document.getElementById("footer");
     var bd_footer = document.getElementById("bd_footer");
     $('#page-content').load('templates/fiche_restaurants.html', function () {
-        imgElement.onclick = handleImageClick;
+        window.myApp.imgElement.onclick = handleImageClick;
         $('#header, #footer, #bd_footer, #search-container, .large-button1, .large-button2, .large-button3, .large-button4').show();
         $('#overlay').show();
         bd_footer.style.height = "29%";
     });
-
 });
 
-function redirectAndDisplayRestaurants(){
+function redirectAndDisplayRestaurants() {
     const type = document.getElementById("displayTypeText").innerText;
 
-    sessionStorage.setItem("fiche_restaurants_display", "false")
+    sessionStorage.setItem("fiche_restaurants_display", "false");
 
     $('#page-content').load('templates/fiche_restaurants.html', function () {
-        imgElement.onclick = handleImageClick;
+        window.myApp.imgElement.onclick = handleImageClick;
         $('#header, #footer, #bd_footer, #search-container, .large-button1, .large-button2, .large-button3, .large-button4').show();
         $('#overlay').show();
         bd_footer.style.height = "29%";
 
-        url = config.apiUrl + "/restaurants?type=" + type
-        displayRestaurant(url)
+        url = config.apiUrl + "/restaurants?type=" + type;
+        displayRestaurant(url);
     });
 }
